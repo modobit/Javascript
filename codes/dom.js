96a4d656976f11c8af86772915b8e6cca797ba0f
modo <modobit@users.noreply.github.com>
2021-06-05 15:31:28 -0400

    update

--- codes/dom.js
+++ codes/dom.js
@@ -1,12 +1,17 @@
 
-
+// DOM Manipulation 
 
 const body = document.body
 
 body.append("Hello World")
 
 const div = document.createElement('div')
+const strong = document.createElement('strong')
 
+
+strong.innerText = "Hello Wprld 2"
+
+div.append(strong)
 div.innerText = " This is the new element"
 div.textContent = "This is the text from text content"
 
